550ba4e748e5d343f3425b2fbfaec7837b4f6e0f
Raj <rajkumarbxr78@gmail.com>
2026-02-28 12:53:35 +0530

    set up studentRoutes and multer middleware

--- server/app.js
+++ server/app.js
@@ -5,17 +5,31 @@ import cookieParser from "cookie-parser";
 import { errorMiddleware } from "./middlewares/error.js";
 import authRouter from "./router/userRoutes.js";
 import adminRouter from "./router/adminRoutes.js";
-
+import studentRouter from "./router/studentRoutes.js";
+import { fileURLToPath } from "url";
+import path from "path";
+import fs from "fs";
 
 config();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 
-app.use(cors({
+app.use(
+  cors({
     origin: [process.env.FRONTEND_URL],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
-}));
+  }),
+);
+
+const uploadsDir = path.join(__dirname, "uploads");
+const tempDir = path.join(__dirname, "temp");
+
+if (!fs.existsSync(uploadsDir)) fs.mkdirSync(uploadsDir, { recursive: true });
+if (!fs.existsSync(tempDir)) fs.mkdirSync(tempDir, { recursive: true });
 
 app.use(cookieParser());
 app.use(express.json());
@@ -23,9 +37,8 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/admin", adminRouter);
+app.use("/api/v1/student", studentRouter);
 
 app.use(errorMiddleware);
 
 export default app;
-
-
---
+++ server/router/studentRoutes.js
@@ -0,0 +1,47 @@
+import express from "express";
+import {
+  getStudentProject,
+  submitProposal,
+  uploadFiles,
+  getAvailableSupervisors,
+} from "../controllers/studentController.js";
+import multer from "multer";
+import {
+  isAuthenticated,
+  isAuthorized,
+} from "../middlewares/authMiddleware.js";
+import { upload, handleUploadError } from "../middlewares/upload.js";
+
+const router = express.Router();
+
+router.post(
+  "/project",
+  isAuthenticated,
+  isAuthorized("Student"),
+  getStudentProject,
+);
+
+router.post(
+  "/proposal",
+  isAuthenticated,
+  isAuthorized("Student"),
+  submitProposal,
+);
+
+router.post(
+  "/upload/:projectId",
+  isAuthenticated,
+  isAuthorized("Student"),
+  upload.array("files", 10),
+  handleUploadError,
+  uploadFiles,
+);
+
+router.get(
+  "/fetch-supervisors",
+  isAuthenticated,
+  isAuthorized("Student"),
+  getAvailableSupervisors,
+);
+
+export default router;
